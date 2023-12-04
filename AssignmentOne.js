9b6ae2c5d4f86e53865f04dc941fe1840866c4bc
shamsuddha <shamsuddha@gmail.com>
2023-12-04 07:21:15 +0600

    dd

---
+++ AssignmentOne.js
@@ -0,0 +1,34 @@
+// Function for task one
+function task_1(n) {  
+  let a = 1;
+  for (let i = 1; i <= n; i++) {
+    a = a * i;
+  }
+  console.log(a);
+}
+
+task_1(5);
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+// Function for tast two
+function task_2(a,b){
+  let x = 0;
+  if (b > a) {
+      for (let i = a; i <= b; i++) {
+          x = x + i;
+      }
+  } 
+  else {
+      for (let i = b; i <= a; i++) {
+          x = x + i;
+      }
+  }
+  console.log(x);
+}
+
+task_2(5);
+
+Time Complexity: O(n)
+Space Complexity: O(1)
